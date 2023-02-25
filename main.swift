839662ab3f8701f0ab2bcbdda3b24e6ecc8b5db5
Shahpar Islam <60633168+Islam-shahpar@users.noreply.github.com>
2023-02-25 16:04:14 +0000

    Added new lines of code

--- main.swift
+++ main.swift
@@ -1,1 +1,24 @@
-print("ITP137")
+import Foundation
+import Glibc
+
+// var courseCode = true
+// print(type(of: courseCode))
+// //print(courseCode)
+
+
+// var temp = 0.0
+// temp = Double(readLine()!)!
+// print(temp)
+
+var num: Int
+num = Int(readLine()!)!
+// if num % 2 == 0 {
+//   print(String(num) + " is Even.")
+// } else {
+//   print(String(num) + " is Odd.")
+// }
+
+while num > 0 {
+  print("This is the \(num)")
+  num /= 2
+}
